368b7ff58bad07308573234a14345c35efb16417
kohnakagawa <tsunekou1019@gmail.com>
2017-02-07 15:22:15 +0900

    add haswell version

---
+++ daxpy/run_haswell.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+./daxpy_haswell.out 67108864 1
+./daxpy_haswell.out 33554432 1
+./daxpy_haswell.out 16777216 1
+./daxpy_haswell.out 8388608 1
+./daxpy_haswell.out 4194304 1
+./daxpy_haswell.out 2097152 1
+./daxpy_haswell.out 1048600 1
+./daxpy_haswell.out 524288 1
+./daxpy_haswell.out 262144 1
+./daxpy_haswell.out 139888 1
+./daxpy_haswell.out 65536 1
+./daxpy_haswell.out 32768 1
+
